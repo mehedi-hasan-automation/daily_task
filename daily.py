b36c1aaa8e875671275c0f87e938128e08f7bcf3
unknown <mehedisumon886@gmail.com>
2026-01-08 08:54:15 +0600

    day 3 work

--- daily.py
+++ daily.py
@@ -1,13 +1,2 @@
-name = "YourName"
-age = 20
-country = "Bangladesh"
-is_student = True
-
-print(name)
-print(age)
-print(country)
-print(is_student)
-print(type(name))
-print(type(age))
-print(type(country))
-print(type(is_student))
+age = input ("Enter your age")
+print (type(age))
