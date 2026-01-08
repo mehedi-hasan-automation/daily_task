c26bc9146f39fce7f009d16c1b221226f582f81d
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-08 09:21:08 +0600

    int and input use

--- daily.py
+++ daily.py
@@ -1,9 +1,3 @@
-age = input ("Enter your age")
-print (type(age))
 
-name = "Mehedi"
-print ("My name is:", name)
-
-
-# Day 3: Global email config updated for contribution graph
-print("My contribution setup is now fixed!")
+a = int(input("Enter your age:"))
+print (a)
