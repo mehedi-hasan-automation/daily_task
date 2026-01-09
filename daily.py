7ffa15ba5f2cf5593090b597497eef6e9a08a78e
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-09 22:30:12 +0600

    global variable use

--- daily.py
+++ daily.py
@@ -1,6 +1,7 @@
-# Day 4: Environment Success
-print("Python Version 3.13.6 successfully integrated!")
-
-# একটি ছোট ক্যালকুলেশন
-days_left = 120 - 4
-print(f"চ্যালেঞ্জের আরও {days_left} দিন বাকি আছে।")
+x = "awesome"
+def myfunc():
+    global x
+    x = "fantastic"
+    print ("python is "+x)
+myfunc()
+print ("python is", x)
