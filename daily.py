3b62272fef88da5685bd29c9ab7a309f620682e8
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-10 14:08:48 +0600

    Practicing Python Casting

--- daily.py
+++ daily.py
@@ -1,6 +1,11 @@
-import random
+"""import random
 
 fruits = ["apple", "banana", "mango", "cherry"]
 take = random.choice(fruits)
 print ("This is free for you:")
-print("Please take it",take)
+print("Please take it",take)"""
+
+x = "10"
+y = "20"
+print (x+y)
+print (int(x)+int(y))
