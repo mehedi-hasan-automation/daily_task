1c32080f8b7c403a68bb96acdd8cc23bb2ed9fad
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-10 11:34:11 +0600

    complex number use

--- daily.py
+++ daily.py
@@ -1,8 +1,12 @@
-x = "awesome"
+"""x = "awesome"
 def function():
     global x
     x = "fantastic"
     print ("python is "+x)
 function()
 print ("python is", x)
-print ( This is my trick comment )
+print ( This is my trick comment )"""
+
+c = 20 + 10j 
+print (c.real)
+print (c.imag)
