c390676202715e03298a648df1f3317f3605b62b
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-10 12:55:45 +0600

    Practicing Random module and randrange

--- daily.py
+++ daily.py
@@ -7,6 +7,16 @@ function()
 print ("python is", x)
 print ( This is my trick comment )"""
 
-c = 20 + 10j 
+"""c = 20 + 10j 
 print (c.real)
-print (c.imag)
+print (c.imag)"""
+
+"""x = complex(2.0)
+print (x) """
+
+"""import random
+print (random.randint(1,10))"""
+import random
+friends = ["Rakib", "Mehedi", "Sabbir"]
+winner = random.choice(friends)
+print("আজকের বিজয়ী:", winner)
