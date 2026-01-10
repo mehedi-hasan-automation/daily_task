02a86c68762fc6da602a8a8a4a9a7de2031ed67b
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-10 13:07:15 +0600

    import random use

--- daily.py
+++ daily.py
@@ -1,22 +1,6 @@
-"""x = "awesome"
-def function():
-    global x
-    x = "fantastic"
-    print ("python is "+x)
-function()
-print ("python is", x)
-print ( This is my trick comment )"""
-
-"""c = 20 + 10j 
-print (c.real)
-print (c.imag)"""
-
-"""x = complex(2.0)
-print (x) """
-
-"""import random
-print (random.randint(1,10))"""
 import random
-friends = ["Rakib", "Mehedi", "Sabbir"]
-winner = random.choice(friends)
-print("আজকের বিজয়ী:", winner)
+
+fruits = ["apple", "banana", "mango", "cherry"]
+take = random.choice(fruits)
+print ("This is free for you:")
+print("Please take it",take)
