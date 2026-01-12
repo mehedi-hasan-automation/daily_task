ceb746f076e450b769549a5860bcb4da94f9041e
mehedi-hasan-automation <mehedi.hasan.automation@gmail.com>
2026-01-12 21:09:16 +0600

    backslash

--- daily.py
+++ daily.py
@@ -1,4 +1,2 @@
-# Escape characters practice
-message = "১. পাইথন শিখছি \"সহজভাবে\"\n২. আজ নতুন কিছু শিখলাম\tমাশাআল্লাহ!"
-
-print(message)
+txt = "This will insert one \\ (backslash)."
+print(txt) 
